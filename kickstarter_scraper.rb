0744d64a1eba508822d5641d75aa7b8bee145c62
Jasmine Chen <jasmine1226@gmail.com>
2019-06-22 06:24:43 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,7 +12,12 @@ def create_project_hash
 
   kickstarter.css("li.project.grid_4").each do |project|
     title = kickstarter.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {}
+    projects[title.to_sym] = {
+      :image_link => kickstarter.css("div.project-thumbnail a").attribute("src").value
+      :description => kickstarter.css("p.bbcard_blurb").text
+      :location => kickstarter.css("ul.project-meta li a span.location-name").text
+      :percent_funded => kickstarter.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+    }
 
 
   end
