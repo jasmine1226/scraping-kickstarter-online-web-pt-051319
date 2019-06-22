46b5f9e2be010bc419b146f423745a3c139b284a
Jasmine Chen <jasmine1226@gmail.com>
2019-06-22 06:21:32 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,7 +12,7 @@ def create_project_hash
 
   kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
-    
+
   end
 
   projects
@@ -25,4 +25,4 @@ create_project_hash
 # image link: kickstarter.css("div.project-thumbnail a").attribute("src").value
 # description: kickstarter.css("p.bbcard_blurb").text
 # location: kickstarter.css("ul.project-meta li a span.location-name").text
-# percent_funded: kickstarter.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i 
+# percent_funded: kickstarter.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
