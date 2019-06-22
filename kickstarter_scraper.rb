050bf0822f8b291b2ad469274750c9f58235120e
Jasmine Chen <jasmine1226@gmail.com>
2019-06-22 06:04:59 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,12 @@
 # require libraries/modules here
 require "nokogiri"
+require "pry"
 
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = nokogiri::HTML(open(html))
+
+  binding.pry
 end
